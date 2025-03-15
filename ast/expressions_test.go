73aa2d1c0f93d95018c2b1b1e5eaadc99b6a529e
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-03-15 19:05:35 -0400

    assign lexeme to native type parsing to new literal function

--- ast/expressions_test.go
+++ ast/expressions_test.go
@@ -14,33 +14,33 @@ func TestExpressionsStrings(t *testing.T) {
 
 	tcs := []testCase{
 		{
-			expr:     NewBinary(NewLiteral(10), lexer.Plus, NewLiteral(12)),
+			expr:     NewBinary(NewLiteral("10", lexer.Number), lexer.Plus, NewLiteral("12", lexer.Number)),
 			expected: "(10 + 12)",
 		},
 		{
-			expr:     NewUnary(lexer.Minus, NewLiteral(5)),
+			expr:     NewUnary(lexer.Minus, NewLiteral("5", lexer.Number)),
 			expected: "(-5)",
 		},
 		{
-			expr:     NewGroup(NewLiteral("Hello world")),
+			expr:     NewGroup(NewLiteral("Hello world", lexer.String)),
 			expected: "(Hello world)",
 		},
 		{
-			expr:     NewLiteral(20),
+			expr:     NewLiteral("20", lexer.Number),
 			expected: "20",
 		},
 		{
 			expr: NewGroup(
 				NewBinary(
-					NewLiteral(10),
+					NewLiteral("10", lexer.Number),
 					lexer.Star,
 					NewBinary(
 						NewUnary(
 							lexer.Minus,
-							NewLiteral(20),
+							NewLiteral("20", lexer.Number),
 						),
 						lexer.Slash,
-						NewLiteral(8),
+						NewLiteral("8", lexer.Number),
 					),
 				),
 			),
@@ -65,19 +65,19 @@ func TestExpressionsComputing(t *testing.T) {
 
 	testCases := []testCase{
 		{
-			expr:     NewBinary(NewLiteral(10.0), lexer.Slash, NewLiteral(5.0)),
+			expr:     NewBinary(NewLiteral("10.0", lexer.Number), lexer.Slash, NewLiteral("5.0", lexer.Number)),
 			expected: 2.0,
 		},
 		{
-			expr:     NewUnary(lexer.Minus, NewLiteral(10.0)),
+			expr:     NewUnary(lexer.Minus, NewLiteral("10.0", lexer.Number)),
 			expected: -10.0,
 		},
 		{
-			expr:     NewGroup(NewLiteral(20.0)),
+			expr:     NewGroup(NewLiteral("20.0", lexer.Number)),
 			expected: 20.0,
 		},
 		{
-			expr:     NewLiteral(1.0),
+			expr:     NewLiteral("1.0", lexer.Number),
 			expected: 1.0,
 		},
 	}
